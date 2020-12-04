a590a21743fa0e91c469d7365962eec41a2154cd
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2020-12-03 23:49:09 -0800

    Remove Indonesian lang letters

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,7 +45,7 @@ class MyDocument extends Document<Props> {
     const page = pageProps.page.selectedPage
 
     return (
-      <html lang="id">
+      <html lang="en">
         <Head>
           <meta charSet="utf-8" />
           {/* Use minimum-scale=1 to enable GPU rasterization */}
