3484c195d541d8a3e7a5bc9aaa3145a76898fdbe
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2020-12-29 11:10:50 -0800

    FEATURE: Add sign in page UI from material-ui templates

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,8 +1,17 @@
-import { Typography } from "@material-ui/core"
+import Avatar from "@material-ui/core/Avatar"
+import Button from "@material-ui/core/Button"
+import Checkbox from "@material-ui/core/Checkbox"
+import FormControlLabel from "@material-ui/core/FormControlLabel"
+import Grid from "@material-ui/core/Grid"
+import Link from "@material-ui/core/Link"
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
+import TextField from "@material-ui/core/TextField"
+import Typography from "@material-ui/core/Typography"
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined"
 import React from "react"
 import { AppContext } from "../components/AppContext"
 import { SpacingPaper } from "../components/atoms"
+import { PageFooter } from "../components/molecules"
 import { HeaderArticleContainer } from "../components/organisms"
 import { Layout } from "../components/templates"
 import { Page } from "../constants"
@@ -11,6 +20,24 @@ import { IPagePayload, PageActions } from "../store/page"
 const useStyles = makeStyles((_: Theme) =>
   createStyles({
     root: {},
+    paper: {
+      padding: _.spacing(4),
+      marginTop: _.spacing(6),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: _.spacing(1),
+      backgroundColor: _.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: _.spacing(1),
+    },
+    submit: {
+      margin: _.spacing(3, 0, 2),
+    },
   })
 )
 
@@ -22,8 +49,65 @@ function Signin(props: Props) {
     <Layout className={classes.root}>
       <HeaderArticleContainer>
         <SpacingPaper>
-          <Typography variant="h5">Sign In Page</Typography>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
         </SpacingPaper>
+        <PageFooter />
       </HeaderArticleContainer>
     </Layout>
   )
